fd74237a61566eb268b818a618b862e553a639c7
Denise Pendleton <dpendl1@hotmail.com>
2018-02-15 00:27:40 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -34,14 +34,14 @@ def turn(board)
     turn(board)
   end
 
-  def turn(board)
-    puts "Please enter 1-9:"
-    input = gets.strip
-    index = input_to_index(input)
-    if valid_move?(board, index)
-    end
-
-end
+#   def turn(board)
+#     puts "Please enter 1-9:"
+#     input = gets.strip
+#     index = input_to_index(input)
+#     if valid_move?(board, index)
+#     end
+# 
+# end
 
 def turn_count(board)
     board.count do |space|
